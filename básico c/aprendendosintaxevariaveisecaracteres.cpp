65abb3ed58ce9b35c62836f200295f157ea5d7c7
Caio Martins <caioeduardoavelino@gmail.com>
2024-08-11 23:39:13 -0300

    Add files via upload

---
+++ básico c/aprendendosintaxevariaveisecaracteres.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+char nome[50];
+char sobrenome[50];
+int idade=2;
+char genero;
+
+int main(){
+printf("Escreva seu primeiro nome:\n");
+scanf("%s", nome);
+printf("Nome digitado:\n");
+
+printf("Escreva seu sobrenome:\n");
+scanf("%s", sobrenome);
+printf("Sobrenome digitado:\n");
+
+printf("Escreva sua idade:\n");
+scanf("%d", idade);
+printf("Idade digitada:\n");
+
+printf("Digite seu genero:\n");
+scanf("%c", genero);
+printf("Genero digitado\n");	
+
+printf("Info: nome: %s sobrenome: %s idade: %d genero: %c", nome, sobrenome, idade, genero);
+}
